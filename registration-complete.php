c6c044c950b277896c457e21b285ee06389c8b24
Hiten Chowdhary <hiten.iitkgp@gmail.com>
2015-07-31 00:29:03 +0530

    Rename all files to .php

--- registration-complete.php
+++ registration-complete.php
@@ -26,6 +26,7 @@
           </div>
           <div class="col-md-6">
             <a href="./index.php" class="btn btn-success btn-block">Visit Home</a>
+
           </div>
         </div>
       </div>
