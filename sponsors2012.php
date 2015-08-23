47fe6912245fd03a3c70b657102201f62864fb49
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-08-23 10:20:36 +0530

    Add the sponsors pages for 2012, 2013, 2014
    Signed-off-by: Siddharth Kannan <kannan.siddharth12@gmail.com>

---
+++ sponsors2012.php
@@ -0,0 +1,26 @@
+<?php session_start(); ?>
+<html>
+
+<head>
+  <title>13th Annual Alumni Meet &middot; Sponsors</title>
+  <?php include 'topHead.php'; ?>
+
+</head>
+
+<body>
+  <?php include "navbar.php"; ?>
+  <div class="container-fluid" align="center">
+   </br>
+   </br>
+   <h1>Sponsors 2012</h1>
+   </br>
+   </br>
+   <a href="https://www.iocl.com/" target="_blank"><img src="img/spons/2012/1.jpg" width="200px"height="150px"  ></a>
+	 <a href="http://www.infosys.com/pages/index.aspx" target="_blank"><img src="img/spons/2012/2.jpg" width="250px"height="150px"/></a>
+   <a href="http://www.mallcomindia.com/site/index.php" target="_blank"><img src="img/spons/2012/3.jpg" width="200px"height="150px"/></a>
+	 <br>
+	 <br>
+	 <br>
+  </div>
+</body>
+</html>
---
+++ sponsors2014.php
@@ -0,0 +1,59 @@
+<?php session_start(); ?>
+<html>
+
+<head>
+  <title>13th Annual Alumni Meet &middot; Sponsors</title>
+  <?php include 'topHead.php'; ?>
+</head>
+
+<body>
+  <?php include "navbar.php"; ?>
+  <div class="container-fluid" align="center">
+			<h1> Sponsors 2014 </h1>
+			<br>
+			<br>
+			<h2> Co Sponsors </h2>
+		    <a href="http://www.bnpparibas.co.in/en/contact-us/srei-equipment-finance/" target="_blank"><img class="spons" src="img/spons/2014/cosponsor.jpg" width="600px" height=""/></a>
+         <br>
+				 <br>
+
+			<h2> Major Sponsors </h2>
+			  <a href="http://www.hmel.in/" target="_blank"><img class="spons" src="img/spons/2014/major.png" width="200px" height=""/></a>
+         <br>
+				 <br>
+				 <br>
+			<h2> Healthcare Partner </h2>
+			    <a href="http://www.hillemanlabs.org/" target="_blank"><img class="spons" src="img/spons/2014/e3.jpg" width="400px" height=""/></a>
+
+				 <br>
+				 <br>
+			<h2> Event Sponsors </h2>
+                <a href="http://www.bseindia.com/" target="_blank"><img class="spons" src="img/spons/2014/e2.png" width="200px" height=""/></a>
+			    <a href="http://www.wbsedcl.in/irj/go/km/docs/internet/webpage/HomePage.html" target="_blank"> <img class="spons" src="img/spons/2014/e1.png" width="200px" height=""/></a>
+                <a href="https://www.vodafone.in/pages/index.aspx" target="_blank"> <img class="spons" src="img/spons/2014/ev4.jpg" width="200px" height=""/></a>
+                <a href="http://www.vanheusenindia.com/" target="_blank"> <img class="spons" src="img/spons/2014/e5.jpg"width="400px" height=""/></a>
+                <a href="http://www.kalikasteels.com/" target="_blank"><img class="spons" src="img/spons/2014/e6.png"width="200px" height=""/></a>
+
+				 <br>
+				 <br>
+		     <h2> Gift Sponsors </h2>
+                <a href="http://www.gatikwe.com/" target="_blank"><img class="spons" src="img/spons/2014/gg.jpg"width="200px" height=""/></a>
+	            <a href="http://www.indiaskylanterns.com/" target="_blank"> <img class="spons" src="img/spons/2014/lantern.png"width="200px" height=""/></a>
+		    	<a href="http://www.idasystems.net/" target="_blank"> <img class="spons" src="img/spons/2014/mar.jpg"width="200px" height=""/></a>
+
+			    <a href="http://www.iwt.in/" target="_blank"> <img class="spons" src="img/spons/2014/rec.png"width="200px" height=""/></a>
+			    <a href="http://www.coca-colaindia.com/" target="_blank"> <img class="spons" src="img/spons/2014/bev.jpg" width="200px" height=""/></a>
+				<a href="http://www.sardamagnets.com/" target="_blank"><img class="spons" src="img/spons/2014/reg2.jpg"width="200px" height=""/></a>
+
+			    <a href="http://www.tatonka.com/defaulten.aspx" target="_blank"> <img class="spons" src="img/spons/2014/fart.jpg"width="300px" height=""/></a>
+			    <a href="http://www.vanguardworld.com/" target="_blank"> <img class="spons" src="img/spons/2014/fart2.jpg"width="300px" height=""/></a>
+			   <a href="http://www.staedtler-india.com/" target="_blank"> <img class="spons" src="img/spons/2014/reg.png"width="300px" height=""/></a>
+			   <a href="http://www.ssdindia.in/" target="_blank"><img class="spons" src="img/spons/2014/sms.png"width="300px" height=""/></a>
+				 <br>
+				 <br>
+                 <br>
+				 <br>
+  </div>
+</body>
+
+</html>
