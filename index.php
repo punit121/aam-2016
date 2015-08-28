4b3aab7174a3580cbaac8b17f2aa37786fe915a7
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-08-28 21:21:06 +0530

    Add the sponsors carousel to the homepage.
    Squashed commit of the following:
    
    commit d648c88d38f5fef15f0d26550427ef3d85910887
    Author: Siddharth Kannan <kannan.siddharth12@gmail.com>
    Date:   Fri Aug 28 21:20:40 2015 +0530
    
        Fix center align of sponsors carousel.
    
        Signed-off-by: Siddharth Kannan <kannan.siddharth12@gmail.com>
    
    commit 3beb3bced851ca40e71995a9b786751b353f0eda
    Merge: e05e264 1c0111b
    Author: Siddharth Kannan <kannan.siddharth12@gmail.com>
    Date:   Fri Aug 28 10:16:51 2015 +0530
    
        Merge branch 'gh-pages' of https://github.com/punit121/aam-2016 into punit121-gh-pages
    
    commit 1c0111b7ece629a43b3489890bcbf0deebc9b2b5
    Author: punit121 <punit.kumar1661@gmail.com>
    Date:   Fri Aug 28 01:14:06 2015 +0530
    
        changes made
    
    commit 65558fadc9d28b5ed50a035ca0a4af0cbe86f084
    Author: punit121 <punit.kumar1661@gmail.com>
    Date:   Thu Aug 27 23:06:43 2015 +0530
    
        changes done
    
    commit 92a5726a9db8437fb79fac57c39063f57b2d2ac7
    Author: punit121 <punit.kumar1661@gmail.com>
    Date:   Thu Aug 27 18:27:30 2015 +0530
    
        sponsors added on index page
    
    commit f9c34ae4c1340844d211c1570b5cddcf91be4d60
    Author: punit121 <punit.kumar1661@gmail.com>
    Date:   Wed Aug 26 23:13:31 2015 +0530
    
        Sponsors slider added
    
    commit c43e1cf3dc80bc37bd522d164782f0bab193f661
    Merge: ca36538 b73799f
    Author: punit121 <punit.kumar1661@gmail.com>
    Date:   Wed Aug 26 18:51:04 2015 +0530
    
        Merge pull request #1 from students-alumni-cell-iitkgp/gh-pages
    
        index.php

--- index.php
+++ index.php
@@ -4,16 +4,68 @@
 <head>
   <title>13th Annual Alumni Meet &middot; Home</title>
   <?php include 'topHead.php'; ?>
+ <style>
+ .item{
+   margin-left: 40%;
+   margin-right: 40%;
+  }
+ </style>
+ <link href="css/carousel.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
-  <?php include "navbar.php"; ?>
-  <!-- <hr> -->
+  <?php include 'navbar.php'; ?>
+
 
   <div class="container">
-    <div class="col-md-6" id="verticalLine">
-      <img src="img/logo-square.jpg" alt="" class="logo" />
+    <div class="col-md-6 text-center" id="verticalLine">
+      <img class="text-center" src="img/logo-square.jpg" alt="" class="logo" />
+
+      <h2 class="text-center"><b>sponsored by</b></h2>
+
+
+<div id="myCarousel" class="carousel carousel-fade text-center" data-ride="carousel">
+
+
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img class="img-responsive" src="img/spons/sponsors/e3.png"width="148" height="148">
     </div>
+  <div class="item">
+      <img class="img-responsive" src="img/spons/sponsors/e2.png"width="148" height="148">
+    </div>
+    <div class="item">
+      <img class="img-responsive" src="img/spons/sponsors/g11.png" width="250" height="300px">
+    </div>
+    <div class="item">
+      <img class="img-responsive"src="img/spons/sponsors/e5.png"width="148" height="148">
+    </div>
+  <div class="item">
+      <img class="img-responsive"src="img/spons/sponsors/luxuryhotelpartner.png"width="200" height="200">
+    </div>
+  <div class="item">
+      <img class="img-responsive"src="img/spons/sponsors/g8.png"width="148" height="148">
+    </div>
+  <div class="item">
+      <img src="img/spons/sponsors/strategic.png"width="148" height="180">
+    </div>
+  <div class="item">
+      <img src="img/spons/sponsors/g9.png"width="148" height="148">
+    </div>
+  <div class="item">
+      <img class="img-responsive"src="img/spons/sponsors/e7.png"width="200" height="400">
+    </div>
+  <div class="item">
+      <img class="img-responsive"src="img/spons/sponsors/e5.png"width="150" height="300">
+    </div>
+  <div class="item">
+      <img class="img-responsive"src="img/spons/sponsors/e4.png"width="148" height="148">
+    </div>
+ </div>
+ </div>
+ </div>
+
+
 
     <div class="col-md-6">
       <div class="form-group" id="form">
@@ -46,12 +98,13 @@
           </div>
         </div>
         <img src="img/b.png" alt="" id="quotes" class="pull-right" />
-        <!-- <div class="component"></div> -->
+
       </div>
     </div>
   </div>
 </body>
 <script src="js/typed.min.js"></script>
+
 <script src="js/main.js"></script>
 
 </html>
