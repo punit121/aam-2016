7a07f5b97067bb233488d3a8ce3f65c2098ccc3c
Hiten Chowdhary <hiten.iitkgp@gmail.com>
2015-08-31 21:18:31 +0530

    unset position changed in updatePassword.php

--- updatePassword.php
+++ updatePassword.php
@@ -4,13 +4,13 @@ $password = $_POST["password"];
 $password = hash('sha256',$password);
 if (isset($_SESSION["forgotPasswordEmail"])) {
   $email = $_SESSION["forgotPasswordEmail"];
+  unset($_SESSION["forgotPasswordEmail"]);
 } else {
   header('Location: ./index.php?alert=Session timed out. Please try again.');
 }
 include 'connection.php';
 $sql = "UPDATE users SET password=$password WHERE email=$email";
 if ($connection->query($sql)) {
-  unset($_SESSION["forgotPasswordEmail"]);
   $connection->close();
   $_SESSION["email"] = $email;
   header('Location: ./login-complete.php');
