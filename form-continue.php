c7fce6ae814cd0539c4b276f660690fdc0a4a7c0
Utsav Agarwal <uagarwal744@gmail.com>
2016-01-04 23:02:05 +0530

    Add the travel and accomodation form.

---
+++ form-continue.php
@@ -0,0 +1,55 @@
+<?php
+if ($_SESSION["email"] != "" || empty($_POST['button'])) {
+	header('Location:form.php');
+}
+mysql_connect("localhost","root","") or die();
+mysql_select_db("meetform") or die();
+
+$email=$_SESSION['email'];
+$arrDate=$_POST['arrDate'];
+$arrTime=$_POST['arrTime'];
+$arrSt=$_POST['arrSt'];
+$trainName=$_POST['trainName'];
+$trainNo=$_POST['trainNo'];
+$accNo=$_POST['accNo'];
+$iscab=$_POST['iscab'];
+$cabWhere=$_POST['cabWhere'];
+$cabWhen=$_POST['cabWhen'];
+$cabDate=$_POST['cabDate'];
+$depDate=$_POST['depDate'];
+$depTime=$_POST['depTime'];
+$depSt=$_POST['depSt'];
+$dtrainName=$_POST['dtrainName'];
+$dtrainNo=$_POST['dtrainNo'];
+$iscab2=$_POST['iscab2'];
+$acabPref=$_POST['acabPref'];
+$dcabPref=$_POST['dcabPref'];
+
+$accNo2=$_POST['accNo2'];
+$accName1=$_POST['accName1'];
+$accRel1=$_POST['accRel1'];
+$accAge1=$_POST['accAge1'];
+$accName2=$_POST['accName2'];
+$accRel2=$_POST['accRel2'];
+$accAge2=$_POST['accAge2'];
+$accName3=$_POST['accName3'];
+$accRel3=$_POST['accRel3'];
+$accAge3=$_POST['accAge3'];
+$prefName=$_POST['prefName'];
+$prefYear=$_POST['prefYear'];
+$prefDep=$_POST['prefDep'];
+$prefHall=$_POST['prefHall'];
+
+$query1="INSERT INTO travel set email='$email', arrivaldate='$arrDate', arrivalstation='$arrSt', trainname='$trainName', trainno='$trainNo', accompanyno='$accNo', 
+cabreq='$iscab', cabfrom='$cabWhere', cabat='$cabWhen',arrivaltime='$arrTime' , cabdate='$cabDate' , arrivalcabpref='$acabPref' , departdate='$depDate' ,
+ departtime='$depTime' , departtrain='$dtrainName' , departtrainno='$dtrainNo' , departstation='$depSt' , depcabreq='$iscab2' , depcabpref='$dcabPref' ";
+
+$result1=mysql_query($query1);
+
+$query2="INSERT INTO accommodation SET email='$email', accompanyno='$accNo2', accname1='$accName1', accrel1='$accRel1', accage1='$accAge1', accname2='$accName2', 
+accrel2='$accRel2', accage2='$accAge2', accname3='$accName3', accrel3='$accRel3', accage3='$accAge3', prefname='$prefName', prefyear='$prefYear', prefdep='$prefDep', 
+prefhall='$prefHall' ";
+
+$result2=mysql_query($query2);
+
+?>
---
+++ form.php
@@ -0,0 +1,210 @@
+<?php
+ if ($_SESSION["email"] != "") {
+   header('Location: ./signin.php');
+   exit();
+ }
+ ?>
+<html>
+
+<head>
+  <title>13th Annual Alumni Meet &middot; Logged in!</title>
+  <?php include 'topHead.php'; ?>
+</head>
+
+<body>
+
+  <div class="container-fluid">
+  	<form action="form-continue.php" method="post">
+      <h2> Travel Details </h2>
+      <div class="form-group">
+        <label for="Name">Arrival Date</label>
+        <select name="arrDate" class="form-control">
+        	<option>13th January 2016</option>
+        	<option>14th January 2016</option>
+        	<option>15th January 2016</option>
+        	<option>16th January 2016</option>
+        </select>
+      </div>
+      <div class="form-group">
+        <label for="Name">Arrival Time</label>
+        <input class="form-control" type="time" placeholder="Arrival Time" name="arrTime">
+      </div>
+      <div class="form-group">
+        <label for="Name">Arrival Station/Airport</label>
+        <input class="form-control" type="text" placeholder="CCU/Howrah/Kharagpur/Other" name="arrSt">
+      </div>
+      <div class="form-group">
+        <label for="Name">Train/Flight Name</label>
+        <input class="form-control" type="text" placeholder="Train/Flight Name" name="trainName">
+      </div>
+      <div class="form-group">
+        <label for="Name">Train/Flight Number</label>
+        <input class="form-control" type="text" placeholder="Train/Flight Number" name="trainNo">
+      </div>
+      <div class="form-group">
+        <label for="Name">Number of Accompanying Person</label>
+        <input class="form-control" type="number" placeholder="No of Accompanying Person" name="accNo">
+      </div>
+      <div class="form-group">
+        <label for="Name">Do you require a cab from Kolkata?</label>
+        <select name="iscab" class="form-control" onchange="checkCab(this)">
+        	<option>N</option>
+        	<option>Y</option>
+        </select>
+      </div>
+      <div class="form-group" style="display:none; margin-left:5%" id="ifcab">
+        <label for="Name">If Cab is required:</label><br>
+        <label for="Name">Pickup Destination</label>
+        <input class="form-control" type="text" placeholder="From where you require the cab" name="cabWhere">
+        <label for="Name">Pickup Date</label>
+        <input class="form-control" type="date" placeholder="Date when the cab is required" name="cabDate">
+        <label for="Name">Pickup Time</label>
+        <input class="form-control" type="time" placeholder="Time when the cab is required" name="cabWhen">
+        <label for="Name">Cab Preference</label>
+        <select name="acabPref" class="form-control">
+        	<option>Swift dezire</option>
+        	<option>Scorpio</option>
+        	<option>Honda City</option>
+        	<option>Indigo</option>
+        </select>
+      </div>
+      <br><br>
+       <div class="form-group">
+        <label for="Name">Departure Date</label>
+        <select name="depDate" class="form-control">
+        	<option>17th January 2016</option>
+        	<option>18th January 2016</option>
+        	<option>19th January 2016</option>
+        </select>
+      </div>
+      <div class="form-group">
+        <label for="Name">Departure Time</label>
+        <input class="form-control" type="time" placeholder="Arrival Time" name="depTime">
+      </div>
+      <div class="form-group">
+        <label for="Name">Departure Station/Airport</label>
+        <input class="form-control" type="text" placeholder="CCU/Howrah/Kharagpur/Other" name="depSt">
+      </div>
+      <div class="form-group">
+        <label for="Name">Train/Flight Name</label>
+        <input class="form-control" type="text" placeholder="Train/Flight Name" name="dtrainName">
+      </div>
+      <div class="form-group">
+        <label for="Name">Train/Flight Number</label>
+        <input class="form-control" type="text" placeholder="Train/Flight Number" name="dtrainNo">
+      </div>
+      <div class="form-group">
+        <label for="Name">Do you require a cab to Kolkata?</label>
+        <select name="iscab2" class="form-control" onchange="checkCab2(this)">
+        	<option>N</option>
+        	<option>Y</option>
+        </select>
+      </div>
+      <div class="form-group" style="display:none; margin-left:5%" id="ifcab2">
+      <label for="Name">If Cab is required</label><br>
+        <label for="Name">Cab Preference</label>
+        <select name="dcabPref" class="form-control">
+        	<option>Swift dezire</option>
+        	<option>Scorpio</option>
+        	<option>Honda City</option>
+        	<option>Indigo</option>
+        </select>
+      </div>
+      <hr>
+
+      <h2> Accommodation Details </h2>
+      <div class="form-group">
+        <label for="Name">Number of Accompanying Person</label>
+        <select class="form-control" name="accNo2" onchange="checkAcc(this)">
+        	<option>0</option>
+        	<option>1</option>
+        	<option>2</option>
+        	<option>3</option>
+        </select>
+      </div>
+      <div class="form-group" style="display:none; margin-left:5%" id="ifacc1">
+      	<label for="Name">Accompanying Persons</label><br>
+      	<label for="Name">1.</label><br>
+        <label for="Name">Name of Person</label>
+        <input class="form-control" type="text" placeholder="Name" name="accName1">
+        <label for="Name">Relationship with the Person</label>
+        <input class="form-control" type="text" placeholder="Relationship" name="accRel1">
+        <label for="Name">Age</label>
+        <input class="form-control" type="number" placeholder="Age" name="accAge1">
+      </div>
+      <div class="form-group" style="display:none; margin-left:5%" id="ifacc2">
+      	<label for="Name">2.</label><br>
+        <label for="Name">Name of Person</label>
+        <input class="form-control" type="text" placeholder="Name" name="accName2">
+        <label for="Name">Relationship with the Person</label>
+        <input class="form-control" type="text" placeholder="Relationship" name="accRel2">
+        <label for="Name">Age</label>
+        <input class="form-control" type="number" placeholder="Age" name="accAge2">
+      </div>
+      <div class="form-group" style="display:none; margin-left:5%" id="ifacc3">
+      	<label for="Name">3.</label><br>
+        <label for="Name">Name of Person</label>
+        <input class="form-control" type="text" placeholder="Name" name="accName3">
+        <label for="Name">Relationship with the Person</label>
+        <input class="form-control" type="text" placeholder="Relationship" name="accRel3">
+        <label for="Name">Age</label>
+        <input class="form-control" type="number" placeholder="Age" name="accAge3">
+      </div>
+      
+      <div class="form-group" style="display:block; margin-left:5%" id="ifacc0">
+      <label>Preferred Alumni You Want to Share the room with:<br></label>
+        <label for="Name">Name</label>
+        <input class="form-control" type="text" placeholder="Name of preferred person" name="prefName">
+        <label for="Name">Year of Graduation:</label>
+        <input class="form-control" type="number" placeholder="Year of Graduation" name="prefYear">
+        <label for="Name">Department</label>
+        <input class="form-control" type="text" placeholder="Department of preferred person" name="prefDep">
+        <label for="Name">Hall</label>
+        <input class="form-control" type="text" placeholder="Hall of preferred person" name="prefHall">
+      </div>
+      <button type="submit" class="btn btn-success btn-lg btn-block" name="button">SUBMIT</button>
+    </form>
+
+  </div>
+
+
+  <script type="text/javascript">
+  	function checkCab(x) {
+  		if(x.options[x.selectedIndex].text=="Y") {
+  			document.getElementById("ifcab").style.display="block";
+  		}
+  		else {
+  			document.getElementById("ifcab").style.display="none";
+  		}
+  	}
+
+  	function checkCab2(x) {
+  		if(x.options[x.selectedIndex].text=="Y") {
+  			document.getElementById("ifcab2").style.display="block";
+  		}
+  		else {
+  			document.getElementById("ifcab2").style.display="none";
+  		}
+  	}
+
+  	function checkAcc(x) {
+  		var n=x.options[x.selectedIndex].text;
+  		var y= new Array("ifacc1","ifacc2","ifacc3");
+  		var i;
+  		if(n>0) {
+  			document.getElementById("ifacc0").style.display="none";
+  		}
+  		if(n==0) {
+  			document.getElementById("ifacc0").style.display="block";
+  		}
+  		for(i=0;i<n;i++) {
+  			document.getElementById(y[i]).style.display="block";
+  		}
+  		for(i=n;i<3;i++) {
+  			document.getElementById(y[i]).style.display="none";
+  		}
+  	}
+  </script>
+
+</body>
+</html>
